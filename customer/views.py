8866eb5498b33526b6cdfb2f82b416ddc896a7dd
amigo <amigosp@yandex.ru>
2021-05-27 22:21:15 +0300

    Добавлен метод create_auth_cart в класс RegistrationCustomer

--- customer/views.py
+++ customer/views.py
@@ -4,9 +4,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.conf import settings
 
 # Create your views here.
 from .forms import RegistrationCustomerForm
+from cart.cart import Cart
+from cart.models import CartAuthCustomer
+import json
 
 
 class RegistrationCustomer(CreateView):
@@ -18,10 +22,16 @@ class RegistrationCustomer(CreateView):
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
+            session_cart = Cart(self.request)
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             form.save()
             user = authenticate(username=username, password=password)
             login(request, user)
+            self.create_auth_cart(session_cart)
             return HttpResponseRedirect(self.success_url)
         return render(request, self.template_name, {'form': form})
+
+    def create_auth_cart(self, session_cart):
+        customer_cart = session_cart.cart
+        CartAuthCustomer.objects.create(customer=self.request.user, cart_customer=json.dumps(customer_cart))
