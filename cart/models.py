c2044a3d91e99609325906acb915be0470fee78b
amigo <amigosp@yandex.ru>
2021-05-27 22:15:24 +0300

    Добавлена модель корзины для зарегистрированного пользователя

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class CartAuthCustomer(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart_customer = models.TextField(verbose_name='Корзина')
+
+    class Meta:
+        verbose_name = 'Корзина пользователя'
+        verbose_name_plural = 'Корзины пользователей'
